79d314ad1c609f4fa23fbf23564b03097e9efabb
ArsalanSikander <arsalansikander0216@gmail.com>
2025-04-21 12:20:15 +0500

    Socket logic 3

---
+++ webApp/collab-sphere/src/app/components/socketProvider.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+// this is to be used in the layout and will provide socket connectivity to the webapp
+
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+
+interface SocketContextType {
+    socket: Socket | null;
+}
+
+const SocketContext = createContext<SocketContextType | undefined>(undefined);
+
+export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+
+    const [socket, setSocket] = useState<Socket | null>(null);
+
+
+    useEffect(() => {
+        async function setupSocket() {
+            let connString = `${process.env.NEXT_PUBLIC_CURR_IP}`;
+            console.log("My own ip is: " + process.env.NEXT_PUBLIC_CURR_IP)
+            console.log("Socket server will use this is connString: " + process.env.NEXT_PUBLIC_CURR_IP);
+
+            const socketInstance = io(process.env.NEXT_PUBLIC_SOCK_IP,
+                {
+                    transports: ["websocket"]
+                });
+
+            setSocket(socketInstance);
+
+            return () => {
+                socketInstance.disconnect();
+            };
+        }
+
+        setupSocket();
+
+
+    }, []);
+
+    return (
+        <SocketContext.Provider value={{ socket }}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
+export const useSocket = (): Socket | null => {
+    const context = useContext(SocketContext);
+    if (!context) {
+        throw new Error('useSocket must be used within a SocketProvider');
+    }
+    return context.socket;
+};
